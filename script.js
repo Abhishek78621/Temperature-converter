4dad7262ee549a0c5c5fbf8a5eda91ee377be25e
Abhishek <7shubhamsc@gmail.com>
2024-02-10 14:37:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let cels= document.getElementById('cel');
+   let feht=document.getElementById('feh');
+   
+   cels.addEventListener('input',function(){
+    let c= this.value;
+     let f=(c * 9/5) + 32 ;
+     if(!Number.isInteger(f)){
+       f=f.toFixed(4);
+     }
+     feht.value=f;
+   });
+   feht.addEventListener('input',function(){
+    let f= this.value;
+    let c=(f-32)*5/9;
+    if(!Number.isInteger(c)){
+      c=c.toFixed(4);
+    }
+    cels.value=c;
+   });
